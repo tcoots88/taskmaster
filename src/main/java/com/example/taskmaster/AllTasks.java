7b51a770c8489907ba088232504fe325df882802
tcoots88 <travis.coots@yahoo.com>
2020-02-11 12:55:19 -0800

    completed TaskMaster lab with readme and image

---
+++ src/main/java/com/example/taskmaster/AllTasks.java
@@ -0,0 +1,14 @@
+package com.example.taskmaster;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AllTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_tasks);
+    }
+}
--- src/main/java/com/example/taskmaster/MainActivity.java
+++ src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addTask = findViewById(R.id.button);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent addTaskIntent = new Intent(MainActivity.this, AddTask.class);
+                MainActivity.this.startActivity(addTaskIntent);
+            }
+        });
+
+        Button allTaskButton = findViewById(R.id.button2);
+        allTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent allTasksIntent = new Intent(MainActivity.this, AllTasks.class);
+                MainActivity.this.startActivity(allTasksIntent);
+            }
+        });
     }
+
+
 }
